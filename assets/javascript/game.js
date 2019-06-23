862a875bfd9d4041c172c3a6ed70126ce5ae6bc7
fwilbor <franklin.cfg@gmail.com>
2019-06-23 04:04:25 -0400

    save changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+
+    // jQuery Ready Function waits for the document to complete loading before initiating JavaScript
+    $(document).ready(function() {
+
+     
+        // Create a loop that generates a Random Number between 19-120.
+        
+        }
+  
+            // Add to the Random number.
+            randomNumber = random + randomNumber;
+          }
+  
+          // Then prepend the Random number to the top of our random-number div.
+          $("#random-number").prepend("<br><hr>" + randomNumber);
+        });   
+        
+        
+        
+        
+        
+        
+        // When random-button (Crystal) is clicked...
+      $("#random-button").on("click", function() {
+
+        // ...create a string which will hold the Random number.
+        var lottoNumber = "";
+
+    });
