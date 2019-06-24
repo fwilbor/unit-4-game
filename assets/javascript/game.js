e02633ba3f1b5d404e61ed51fcaa6351c4d481f0
fwilbor <franklin.cfg@gmail.com>
2019-06-24 17:22:29 -0400

    Javascript File uploaded made function and set variables for gameplay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,16 @@
-// PseudoCode Instructions Follow Step By Step
-
-// jQuery Ready Function waits for the document to complete loading before initiating JavaScript
-$(document).ready(function () { },
-
-  // When random-button (Click Me) is clicked... Game Starts
-  $("#random-button").on("click", function () {
-
-    // Add to the Random number.
-
-    //  Prepend the Random number to the top of our random-number div.
-    $("#random-number").prepend("<br><hr>" + randomNumber);  
+alert('our JS is linked!');
 
 
+var winCount = 0;
+var lossCount = 0;
 
+var checkWin = function(){
+  if(currentScore > targetscore){
+    alert('you lost');
+    lossCount++;
+    $(#LOSS DIV).text(lossCount);
+    startGame();
+  } else if (currentScore === targetScore){
 
-//   Click Game to Start and Generate Random Number between 19-120 .text to randomNumber <div> 
-    let getRandomNumber = function (start, range) {
-
-        let getRandom = Math.floor((Math.random() * range) + start);
-
-        while (getRandom > range) {
-
-            getRandom = Math.floor((Math.random() * range) + start);
-
-        }
-        return getRandom;
-        
-       }
-       ,console.log(getRandomNumber(19, 120)));
-
-    
-   
-    
-    })
-
-
-    // Run For Loop to Create 4 Crystals that produce Random Hidden Value between 1 - 12 
-
-
-
-
-
-
-
-
-// ...create a string which will hold the Random number.
-var lottoNumber = "";
-
-    });
+  }
+}
