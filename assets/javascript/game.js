cbf334c75d254ed54c4bf4c02b37550788bb4358
fwilbor <franklin.cfg@gmail.com>
2019-06-24 04:04:38 -0400

    add randomNumber 19-120 to randomNumber div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,50 @@
+// PseudoCode Instructions Follow Step By Step
 
-    // jQuery Ready Function waits for the document to complete loading before initiating JavaScript
-    $(document).ready(function() {
+// jQuery Ready Function waits for the document to complete loading before initiating JavaScript
+$(document).ready(function () { },
 
-     
-        // Create a loop that generates a Random Number between 19-120.
-        
+  // When random-button (Click Me) is clicked... Game Starts
+  $("#random-button").on("click", function () {
+
+    // Add to the Random number.
+
+    //  Prepend the Random number to the top of our random-number div.
+    $("#random-number").prepend("<br><hr>" + randomNumber);  
+
+
+
+
+//   Click Game to Start and Generate Random Number between 19-120 .text to randomNumber <div> 
+    let getRandomNumber = function (start, range) {
+
+        let getRandom = Math.floor((Math.random() * range) + start);
+
+        while (getRandom > range) {
+
+            getRandom = Math.floor((Math.random() * range) + start);
+
         }
-  
-            // Add to the Random number.
-            randomNumber = random + randomNumber;
-          }
-  
-          // Then prepend the Random number to the top of our random-number div.
-          $("#random-number").prepend("<br><hr>" + randomNumber);
-        });   
+        return getRandom;
         
-        
-        
-        
-        
-        
-        // When random-button (Crystal) is clicked...
-      $("#random-button").on("click", function() {
+       }
+       ,console.log(getRandomNumber(19, 120)));
 
-        // ...create a string which will hold the Random number.
-        var lottoNumber = "";
+    
+   
+    
+    })
+
+
+    // Run For Loop to Create 4 Crystals that produce Random Hidden Value between 1 - 12 
+
+
+
+
+
+
+
+
+// ...create a string which will hold the Random number.
+var lottoNumber = "";
 
     });
