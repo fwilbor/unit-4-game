b3fc920e09fb2c8ea8c0b14540b8f7de4ee33d21
fwilbor <franklin.cfg@gmail.com>
2019-06-24 21:39:47 -0400

    add data values to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,13 @@ alert('our JS is linked!');
 var winCount = 0;
 var lossCount = 0;
 
-var checkWin = function(){
-  if(currentScore > targetscore){
-    alert('you lost');
-    lossCount++;
-    $(#LOSS DIV).text(lossCount);
-    startGame();
-  } else if (currentScore === targetScore){
+// var checkWin = function(){
+//   if(currentScore > targetscore){
+//     alert('you lost');
+//     lossCount++;
+//     $(#LOSS DIV).text(lossCount);
+//     startGame();
+//   } else if (currentScore === targetScore){
 
-  }
-}
+//   }
+// }
